3c3a59813fc3d050b50a74fe65cd7fb2eb824179
Shawn Cook <scook@theonion.com>
2013-12-11 14:44:00 -0600

    Positioning of share tools

--- videojs.sharetools.js
+++ videojs.sharetools.js
@@ -45,7 +45,7 @@
         var overlay = document.createElement("div");
         overlay.className = "sharetools-overlay";
 
-        overlay.innerHTML = '<a href="#fb"><span>Facebook</span></a> <a href="#tw"><span>Twitter</span></a> <a href="#em"><span>Embed</span></a> <a href="#close" class="close"><span>Close</span></a>';
+        overlay.innerHTML = '<div class="sharetool"><a href="#fb"><span>Facebook</span></a> <a href="#tw"><span>Twitter</span></a> <a href="#em"><span>Embed</span></a></div> <a href="#close" class="close"><span>Close</span></a>';
 
         overlay.getElementsByClassName('close')[0].onclick = shareTools.teardown;
         player.el().appendChild(overlay);
