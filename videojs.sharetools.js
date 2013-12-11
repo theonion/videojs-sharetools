2f101b930e1064eafd317fc33fe7778d894d8910
Chris Sinchok <csinchok@theonion.com>
2013-12-11 15:48:00 -0600

    Embed code support

--- videojs.sharetools.js
+++ videojs.sharetools.js
@@ -15,13 +15,11 @@
 
   defaults = {
     shareUrl: document.location,
-    sharers: {
-        facebook: function(settings){
-            return "https://www.facebook.com/sharer/sharer.php?u=" + settings.shareUrl;
-        },
-        twitter: function(settings){
-            return "https://twitter.com/share?url=" + settings.shareUrl;
-        }
+    facebook: function(settings){
+        return "https://www.facebook.com/sharer/sharer.php?u=" + settings.shareUrl;
+    },
+    twitter: function(settings){
+        return "https://twitter.com/share?url=" + settings.shareUrl;
     }
   },
 
@@ -44,8 +42,43 @@
         shareTools.open = true;
         var overlay = document.createElement("div");
         overlay.className = "sharetools-overlay";
+        overlay.innerHTML = '<div class="sharetool"></div><a href="#close" class="close"><span>Close</span></a>';
 
-        overlay.innerHTML = '<div class="sharetool"><a href="#fb"><span>Facebook</span></a> <a href="#tw"><span>Twitter</span></a> <a href="#em"><span>Embed</span></a></div> <a href="#close" class="close"><span>Close</span></a>';
+        var shareTool = overlay.getElementsByTagName("div")[0];
+        if(settings.facebook) {
+            var facebook = document.createElement('a');
+            facebook.className = "fb";
+            facebook.href = settings.facebook(settings);
+            facebook.innerHTML = "<span>Facebook</span>";
+            shareTool.appendChild(facebook);
+        }
+        if(settings.twitter) {
+            var twitter = document.createElement('a');
+            twitter.className = "tw";
+            twitter.href = settings.twitter(settings);
+            twitter.innerHTML = "<span>Twitter</span>";
+            shareTool.appendChild(twitter);
+        }
+        if(settings.embed) {
+            var embed = document.createElement('a');
+            embed.className = "em";
+            embed.href = "#em";
+            embed.innerHTML = "<span>Embed</span>";
+            shareTool.appendChild(embed);
+
+            var embedDiv = document.createElement('div');
+            embedDiv.className = "embedtool";
+            embedDiv.innerHTML = "<textarea>" + settings.embed(settings) + "</textarea>";
+            embedDiv.style.display = "none";
+            overlay.appendChild(embedDiv);
+
+            embed.onclick = function(e){
+                shareTool.style.display = "none";
+                embedDiv.style.display = "block";
+                embedDiv.getElementsByTagName('textarea')[0].select();
+            };
+
+        }
 
         overlay.getElementsByClassName('close')[0].onclick = shareTools.teardown;
         player.el().appendChild(overlay);
